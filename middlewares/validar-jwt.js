cb72f687ac6ee2ff1149260a04313e289d971a00
yfigueroa <yfsanchez@gmail.com>
2020-08-11 01:08:41 -0300

    117-Revision de token Middleware

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,32 @@
+const { replaceOne } = require("../models/usuario");
+const jwt = require('jsonwebtoken');
+
+const validarJWT = (req, res, next) => {
+    //Leer token
+    const token = req.header('x-token');
+    if (!token) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No hay token en la peticion'
+        });
+    }
+    try {
+        const { uid } = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(uid);
+        req.uid = uid;
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token no es valido'
+        });
+    }
+    console.log(token);
+
+
+}
+
+module.exports = {
+    validarJWT,
+}
