6f30a6954387c8f5ac04bf70cfc5f09184bf6e16
yfigueroa <yfsanchez@gmail.com>
2020-08-19 09:10:43 -0300

    130-busqueda general de todas las colecciones de forma simultanea

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,13 +1,23 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const Usuario = require('../models/usuario');
+const Hospital = require('../models/hospital');
+const Medico = require('../models/medico');
 
 const getTodo = async(req, res) => {
     const parametro = req.params.parametro;
+    const regex = new RegExp(parametro, 'i');
+    const [usuarios, hospitales, medicos] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Hospital.find({ nombre: regex }),
+        Medico.find({ nombre: regex })
+    ]);
     res.json({
         ok: true,
-        msg: 'busqueda',
-        parametro
+        usuarios,
+        hospitales,
+        medicos
     })
 }
 
